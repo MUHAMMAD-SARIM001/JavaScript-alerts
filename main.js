b3674c03208078c1ff66d70e801df9d749d397af
MUHAMMAD SARIM <muhammad.sarim001100@gmail.com>
2024-12-18 00:43:40 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,106 @@
+// CHP-1 ALERT()----------------->
+
+// Question-1
+
+// alert("Welcome to my website! Hope you have a great experience.");
+
+// Question-2
+
+// alert("Error !Please Enter  a Valid Password");
+
+// Question-3
+
+// alert("Hello Developers \n Welcome To My Assignment")
+
+// Question-4
+
+//  alert("Welcome To JS Land")
+//  alert("Happy Coding")
+
+// Question-5
+
+// console.log("Hello ! I can Run javaScript Through My Web Browser Console")
+
+//   chp-2 VARIABLES FOR STRING-------------->
+
+//  Question-1
+
+// let UserName = "Muhammad SARIM"
+
+//  Question-2
+
+// let myName = " Muhammad SARIM"
+
+//  Question-3
+
+// let message ;
+
+// message = ("Hello World");
+
+// alert(message)
+
+// Question-4
+
+// let studentName = "Muhammad SARIM"; 
+// let age = 18;                 
+// let grade = "A+";             
+// let course = "Modern Web & App Development"; 
+
+// alert("Student Name: " + studentName);
+// alert("Age: " + age);
+// alert("Grade: " + grade);
+// alert("Course: " + course);
+
+// Question-5
+// let  CheesyPizza = ("Pizza\nPizz\nPiz\nPi\nP");
+// alert(CheesyPizza);
+
+// Question-6
+
+// let Email = "muhammad.sarim001100@gmail.com"
+// let Message = "My Email Address Is"+Email;
+//  alert(Message)
+
+//  Question -7
+
+// let book = "A smarter way to learn JavaScript";
+// let message = "I am learning from the book: " + book;
+// alert(message);
+
+// Question-8
+
+//  document.write("<h1>Hello,I am SOFTWARE DEVELOPER!</h1>");
+
+// Question-9
+
+// let Code_Span = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// console.log(Code_Span)
+
+
+
+// CHP-3 VARIABLES FOR NUMBER---------------->
+
+// Question-1
+
+//  let age = 18
+//  alert("Your AGE Is :"+ age)
+
+// Question-2
+
+// let Visitor = "you have visited this website 14 times"
+// alert(Visitor)
+
+// Question-4
+
+// let birthYear =  2006
+// alert("Your BirthYear Is "+birthYear )
+
+// Question-5
+
+// let CustomerName = "John Doe" 
+// let Product =  "T-SHIRTS"
+// let Quantity = 5
+// let BrandName = "SAR CLOTHING STORE"
+// alert (`${CustomerName} Order ${Quantity} ${Product} ON ${BrandName} `)
+
+//   CHP-1,CHP-2,CHP-   3 PRACTICE DONE
